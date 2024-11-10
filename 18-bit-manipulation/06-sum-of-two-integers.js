d7fd5c6bee68166d8e17a74707742f2833489747
Shantanu Mahale <mahaleshantanu3@gmail.com>
2024-11-10 15:13:03 +0530

    Create 06-sum-of-two-integers.js

---
+++ 18-bit-manipulation/06-sum-of-two-integers.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var getSum = function(a, b) {
+    while (b !== 0) {
+        let tmp = (a & b) << 1;
+        a = a ^ b;
+        b = tmp;
+    }
+    return a;
+};
