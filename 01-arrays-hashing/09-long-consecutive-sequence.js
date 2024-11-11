dece15c9bf3d2c04f854f7e6551e41c71db8f566
Shantanu Mahale <mahaleshantanu3@gmail.com>
2024-11-11 23:35:28 +0530

    Update 09-long-consecutive-sequence.js

--- 01-arrays-hashing/09-long-consecutive-sequence.js
+++ 01-arrays-hashing/09-long-consecutive-sequence.js
@@ -3,17 +3,13 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    let numSet = new Set();
+    let numSet = new Set(nums);
     let longest = 0;
-
-    for(let i=0; i<nums.length; i++) {
-        numSet.add(nums[i]);
-    }
 
     for(let n of numSet) {
         if(!numSet.has(n-1)) {
-            let l = 0;
-            while (numSet.has(n+1)) {
+            let l = 1;
+            while (numSet.has(n+l)) {
                 l += 1;
             }
             longest = Math.max(l, longest);
