d32d0c14a0067080fb9528d16f26f1829dbb1569
shantanubr <mahaleshantanu3@gmail.com>
2025-01-16 23:47:20 +0530

    Binary Search

---
+++ 05-binary-search/01-binary-search.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let l=0;
+    let r=nums.length-1;
+
+    while (l<=r) {
+        let mid = Math.floor((l+r)/2);
+        if (target < nums[mid]) {
+            r=mid-1;
+        } else if (target > nums[mid]) {
+            l=mid+1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
+};
